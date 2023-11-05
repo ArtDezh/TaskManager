5fa3c042a55f7fcd79b633c48b93645dcd997298
ArthurD <artur2134@mail.ru>
2023-11-05 15:05:30 +0300

    New_project_

---
+++ src/Manager.java
@@ -0,0 +1,7 @@
+import java.util.HashMap;
+public class Manager {
+    HashMap<Integer, Task> mapTask = new HashMap<>();
+    HashMap<Integer, Epic> mapEpic = new HashMap<>();
+    HashMap<Integer, Subtask> mapSubtask = new HashMap<>();
+
+}
---
+++ src/Task.java
@@ -0,0 +1,38 @@
+public class Task {
+    private String nameTask;
+    private String DescriptionTask;
+    private int idTask;
+    private String statusTask;
+
+    public String getNameTask() {
+        return nameTask;
+    }
+
+    public void setNameTask(String nameTask) {
+        this.nameTask = nameTask;
+    }
+
+    public String getDescriptionTask() {
+        return DescriptionTask;
+    }
+
+    public void setDescriptionTask(String descriptionTask) {
+        DescriptionTask = descriptionTask;
+    }
+
+    public int getIdTask() {
+        return idTask;
+    }
+
+    public void setIdTask(int idTask) {
+        this.idTask = idTask;
+    }
+
+    public String getStatusTask() {
+        return statusTask;
+    }
+
+    public void setStatusTask(String statusTask) {
+        this.statusTask = statusTask;
+    }
+}
